69e3c6995e8e9df8c91e3249775c4027ba55bb95
Lohith Kumar <lkneerukonda@gmail.com>
2024-11-10 00:15:44 -0600

    Docker Image Creation

---
+++ src/main/java/com/example/DockerImageCreation/DockerImageCreationApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerImageCreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerImageCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImageCreationApplication.class, args);
+	}
+
+}
