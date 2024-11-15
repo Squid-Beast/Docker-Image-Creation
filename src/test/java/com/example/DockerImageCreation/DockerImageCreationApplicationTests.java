c00aca9af84642e8a8c9e331d512b6f737349d9e
Lohith Kumar <lkneerukonda@gmail.com>
2024-11-14 22:36:25 -0600

    Docker Image Creation.

---
+++ src/test/java/com/example/DockerImageCreation/DockerImageCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerImageCreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
